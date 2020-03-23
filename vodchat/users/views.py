9fdf9dc70bb23a3eac3e9c1a564e5adcb09fbef6
lacunae173 <leyao.zhang4@gmail.com>
2020-03-22 23:33:52 -0400

    Set up authentication

---
+++ vodchat/users/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from . import forms
+
+#@login_required(login_url='/login')
+def index(request):
+  if not request.user.is_authenticated:
+    return HttpResponseRedirect(reverse('login'))
+  return HttpResponse('Hello ' + request.user.username)
+
+def signup(request):
+    context = {}
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            try:
+              user = User.objects.create_user(
+                form.cleaned_data['username'], 
+                email=form.cleaned_data['email'], 
+                password=form.cleaned_data['password'])
+              return HttpResponseRedirect(reverse('login'))
+            except IntegrityError:
+                form.add_error('username', 'Username is taken')
+
+        context['form'] = form   
+    return render(request, 'users/signup.html', context)
+
+def do_login(request):
+    context = {}
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, 
+              username=form.cleaned_data['username'], 
+              password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                form.add_error(None, 'Unable to log in')
+        context['form'] = form
+    return render(request, 'users/login.html', context)
+
+def do_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+    
